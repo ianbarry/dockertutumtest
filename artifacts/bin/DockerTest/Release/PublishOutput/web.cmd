4fee26b72d6a0efdb44ae3b95d0ab624339cf4a1
ian.barry@bookatable.com <ian.barry@bookatable.com>
2015-10-22 13:37:16 +0100

    Second commit.

--- artifacts/bin/DockerTest/Release/PublishOutput/web.cmd
+++
@@ -1,2 +0,0 @@
-
-@"%~dp0approot\runtimes\dnx-clr-win-x86.1.0.0-beta7\bin\dnx.exe" --appbase "%~dp0approot\src\DockerTest" Microsoft.Dnx.ApplicationHost --configuration Release web %*
---
+++ ef.cmd
@@ -0,0 +1,2 @@
+
+@"dnx.exe" --appbase "%~dp0approot\src\DockerTest" Microsoft.Dnx.ApplicationHost --configuration Release ef %*
